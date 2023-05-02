45c2d7d6c12e2b1b687b83022f3e249f84c948d4
Brian Gaines <Brian.Gaines@sas.com>
2023-05-02 16:33:52 -0400

    First attempt at final module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,6 +1,10 @@
-from . import Expense
+
+# %%
+import timeit
+from budget import Expense
 import matplotlib.pyplot as plt
 
+# %%
 def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
@@ -11,7 +15,36 @@ def main():
     for a, b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print('Sets are NOT equal by subset test')
+    # Timing for loop approach
+    print(timeit.timeit(stmt="expenses.categorize_for_loop()",
+                  setup=
+                  '''
+from budget import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                  ''',
+                  number=100000,
+                  globals=globals()))
+    # Timing for comprehension approach
+    print(timeit.timeit(stmt="expenses.categorize_set_comprehension()",
+                  setup=
+                  '''
+from budget import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                  ''',
+                  number=100000,
+                  globals=globals()))
+    # Pie chart
+    fig,ax=plt.subplots()
+    labels = ['Necessary', 'Food', 'Unnecessary']
+    divided_expenses_sum = []
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append(sum(x.amount for x in category_exps))
+    ax.pie(divided_expenses_sum, labels = labels, autopct = '%1.1f%%')
+    plt.show()
 
 
 if __name__ == "__main__":
     main()
+
